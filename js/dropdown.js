5c4af58c3b3959892b8e1616933acbbff42de4a6
Ulrik-Matemu <advancechronicles@gmail.com>
2025-01-20 23:22:20 +0300

    First Commit

---
+++ js/dropdown.js
@@ -0,0 +1,9 @@
+const dropdownContent = document.getElementById('dropdown-content');
+
+    function displayDropdown() {
+      if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+      } else {
+        dropdownContent.style.display = 'block';
+      }
+    }
