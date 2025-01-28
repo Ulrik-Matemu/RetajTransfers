037a710653127851b0f9e341a49cb681a1985a79
Ulrik-Matemu <advancechronicles@gmail.com>
2025-01-28 13:14:20 +0300

    Added QuickFare calculator

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,9 +1,18 @@
 const dropdownContent = document.getElementById('dropdown-content');
+const pricingDropdownContent = document.getElementById('pricing-dropdown-content');
 
     function displayDropdown() {
       if (dropdownContent.style.display === 'block') {
         dropdownContent.style.display = 'none';
       } else {
         dropdownContent.style.display = 'block';
+      }
+    }
+
+    const displayPricingDropdown = () => {
+      if (pricingDropdownContent.style.display === 'block') {
+        pricingDropdownContent.style.display = 'none';
+      } else {
+        pricingDropdownContent.style.display = 'block';
       }
     }
